8fed4ab449f0ba04ee4f6e8ac8b99627af8c62bc
Korjavin Ivan <korjavin@gmail.com>
2017-02-25 13:47:09 +0500

    1

--- main.go
+++ main.go
@@ -1,44 +1,66 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"github.com/korjavin/go-php-serialize"
-	"golang.org/x/text/encoding/unicode"
-	"os"
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/korjavin/phpserialize2Json"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		val, _ := phpserialize.DecodeWithEncoding(text, unicode.UTF8)
-		switch vtype := val.(type) {
-		default:
-			fmt.Printf("unexpected type %T", vtype)
-		case map[interface{}]interface{}:
-			map1 := val.(map[interface{}]interface{})
-			for k, v := range map1 {
-				if map2, ok := v.(map[interface{}]interface{}); ok {
-					for k, v := range map2 {
-						fmt.Printf("%v %v \n ", k, v)
-					}
+var db *sql.DB
+var stmt *sql.Stmt
+var stmt1 *sql.Stmt
 
-				} else {
-					fmt.Printf("%v %v \n ", k, v)
-				}
-			}
-		}
+const dburl = "root:@unix(/run/mysqld/mysqld.sock)/kino"
+
+func init() {
+	var err error
+	db, err = sql.Open("mysql", dburl)
+	if err != nil {
+		log.Fatalf("mysql: %s", err)
+	}
+	stmt, err = db.Prepare("update kp_people_films set films_json=? where id=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt1, err = db.Prepare("select id, films_ser from kp_people_films where films_json is null limit ?")
+	if err != nil {
+		log.Fatal(err)
 	}
 }
-func traverseMap(key interface{}, val interface{}) {
-	switch val.(type) {
-	default:
-		fmt.Printf("%v %v \n", key, val)
-	case map[interface{}]interface{}:
-		map1 := val.(map[interface{}]interface{})
-		for k, v := range map1 {
-			traverseMap(k, v)
+
+func update(i int) {
+	rows, err := stmt1.Query(i)
+	defer rows.Close()
+	var (
+		id  int
+		ser string
+	)
+	if err != nil {
+		log.Fatalf("mysql: %s", err)
+	}
+	for rows.Next() {
+		if err := rows.Scan(&id, &ser); err != nil {
+			log.Fatal(err)
 		}
+		json1, err := json.DecodeToJSON(ser)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = stmt.Exec(json1, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%d \n", id)
+	}
+}
+func check(c chan<- bool) {
+}
+func main() {
+	for {
+		update(90)
+		time.Sleep(time.Second * 1)
 	}
 }
